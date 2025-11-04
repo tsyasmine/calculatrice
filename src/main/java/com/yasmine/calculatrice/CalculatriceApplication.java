41ca95f471706faa36b0c2081f58625d650a3acc
tsyasmine <tss.yasmine@gmail.com>
2025-11-04 11:03:53 -0500

    ajouter la squelette Spring Boot

---
+++ src/main/java/com/yasmine/calculatrice/CalculatriceApplication.java
@@ -0,0 +1,13 @@
+package com.yasmine.calculatrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasmine/calculatrice/CalculatriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasmine.calculatrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
