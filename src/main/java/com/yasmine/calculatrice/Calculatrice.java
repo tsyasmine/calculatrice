f5021483819e385456808a8947902505f0826260
tsyasmine <tss.yasmine@gmail.com>
2025-11-05 11:01:23 -0500

    ajouter la logique de l'addition, controller et test unitaire

---
+++ src/main/java/com/yasmine/calculatrice/Calculatrice.java
@@ -0,0 +1,8 @@
+package com.yasmine.calculatrice;
+import org.springframework.stereotype.Service;
+@Service
+public class Calculatrice {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
