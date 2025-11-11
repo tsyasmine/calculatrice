e90b20a786b959a34c0e8406cecf79296a47ef56
tsyasmine <tss.yasmine@gmail.com>
2025-11-11 11:16:57 -0500

    ajout des stages de la couverture de code et de qualite de code

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,5 +13,11 @@ pipeline {
                 sh "./gradlew test"
             }
         }
+        stage("Couverture du code") {
+            steps {
+                sh "./gradlew jacocoTestReport"
+                sh "./gradlew jacocoTestCoverageVerification"
+            }
+        }
     }
 }
---
+++ src/test/java/com/yasmine/calculatrice/CalculatriceTest.java
@@ -0,0 +1,14 @@
+package com.yasmine.calculatrice;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatriceTest {
+
+private Calculatrice calculatrice = new Calculatrice();
+
+@Test
+public void testSum() {
+assertEquals(15, calculatrice.sum(10, 5));
+}
+}
